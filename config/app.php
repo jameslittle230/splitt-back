e9739b11921d900346134c45bf31a79f3be9f1a9
James Little <littleguy23@gmail.com>
2019-06-25 17:44:52 -0400

    Add debugbar

--- config/app.php
+++ config/app.php
@@ -166,6 +166,8 @@ return [
          * Package Service Providers...
          */
 
+        Barryvdh\Debugbar\ServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
